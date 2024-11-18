e60df168ca3e4c9a12c8535dde13d6f202185338
joao5142 <joaopauloneto3687@gmail.com>
2024-11-18 17:26:08 -0300

    feat: add components

--- packages/vue/src/components/index.ts
+++ packages/vue/src/components/index.ts
@@ -1,1 +1,14 @@
+export {default as  AppModal } from './partials/AppModal.vue'
+
+export {default as  AppAvatar } from './ui/AppAvatar.vue'
+export {default as  AppBox } from './ui/AppBox.vue'
+export {default as  AppButton } from './ui/AppButton.vue'
+export {default as  AppCard } from './ui/AppCard.vue'
+export {default as  AppCheckbox } from './ui/AppCheckbox.vue'
+export {default as  AppInput } from './ui/AppInput.vue'
+export {default as  AppLoading } from './ui/AppLoading.vue'
+export {default as  AppPost } from './ui/AppPost.vue'
+export {default as  AppText } from './ui/AppText.vue'
+export {default as  AppUserCard } from './ui/AppUserCard.vue'
+
  
