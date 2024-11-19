a9156cd8d20f06bb1d0d2d1975860c797724bd07
joao5142 <joaopauloneto3687@gmail.com>
2024-11-19 01:08:59 -0300

    chore: adjust eslint config

--- packages/eslint-config/eslint.config.js
+++ packages/eslint-config/eslint.config.js
@@ -1,4 +1,3 @@
-import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import eslintConfigPrettier from 'eslint-config-prettier'
@@ -6,59 +5,51 @@ import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import pluginVue from 'eslint-plugin-vue'
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
   {
-    languageOptions: { globals: globals.browser },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
     rules: {
       'no-unused-vars': 'warn',
       'no-console': 'warn',
 
-      "vue/multi-word-component-names": "off",
-      "vue/valid-v-slot": [
-        "error",
+      'vue/multi-word-component-names': 'off',
+      'vue/valid-v-slot': [
+        'error',
         {
           allowModifiers: true,
         },
       ],
-      "vue/html-self-closing": [
-        "error",
+      'vue/html-self-closing': [
+        'error',
         {
           html: {
-            void: "always",
-            normal: "never",
-            component: "any",
+            void: 'always',
+            normal: 'never',
+            component: 'any',
           },
-          svg: "always",
-          math: "always",
+          svg: 'always',
+          math: 'always',
         },
       ],
-      "vue/html-closing-bracket-newline": [
-        "error",
+      'vue/html-closing-bracket-newline': [
+        'error',
         {
-          singleline: "never",
-          multiline: "always",
+          singleline: 'never',
+          multiline: 'always',
         },
       ],
-      "vue/component-name-in-template-casing": [
-        "error",
-        "kebab-case",
+      'vue/component-name-in-template-casing': [
+        'error',
+        'kebab-case',
         {
           registeredComponentsOnly: false,
           ignores: [],
         },
       ],
     },
-    ignores: ['dist', 'build', 'node_modules'],
+    ignores: ['!node_modules/', '!build/', '!dist/'],
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintConfigPrettier,
   eslintPluginPrettierRecommended,
   ...pluginVue.configs['flat/recommended'],
-   
 ]
